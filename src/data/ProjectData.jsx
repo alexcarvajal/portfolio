d25852ec5e42c6c0c999d110cf6e84c44f017d13
alexcarvajal <alexcarvajal@unicauca.edu.co>
2023-10-12 12:37:26 -0500

    Add Twitch API project

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -1,96 +1,105 @@
 // --------------------------------------- Project List
 export const ProjectList = [
-    {
-      img: "/fruits.svg",
-      title: "Online food store",
-      description:
-        "This is an online store that sells food products. Customers can browse the selection of products online and place orders from the comfort of their homes.",
-      tech_stack: ["React","Bootstrap"],
-      github_url: "",
-      demo_url: "https://exampleseven.eolsolutions.com.co/",
-    },
-    {
-      img: "/batabit.svg",
-      title: "Batabit Responsive",
-      description:
-        "Es una aplicación realizada en HTML y CSS con el concepto de mobile first que muestra una landing de trading.",
-      tech_stack: ["HTML", "CSS", "Mobile First"],
-      github_url: "https://github.com/alexcarvajal/MobileFirst",
-      demo_url: "https://mobilityfirst.netlify.app/",
-    },
-    {
-      img: "/Ecommerce.png",
-      title: "Example Ecommerce",
-      description:
-        "Example of an Ecommerce with store functionalities, login, product selection and shopping cart.",
-      tech_stack: ["ReactJs", "Tailwind", "ReactNative"],
-      github_url: "https://github.com/alexcarvajal/Ecommerce",
-      demo_url: "https://commerce-tailw.netlify.app/",
-    },
-  ];
-  
-  // --------------------------------------- Skills
-  
-  export const stackList = [
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
-      name: "HTML",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
-      name: "CSS",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-      name: "JavaScript",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
-      name: "ReactJS",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
-      name: "Git",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
-      name: "React Native",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
-      name: "NodeJS",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
-      name: "Firebase",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-      name: "Python",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
-      name: "C",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
-      name: "MySQL",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
-      name: "Bootstrap",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
-      name: "Material UI",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
-      name: "Figma",
-    },
-    {
-      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
-      name: "VS Code",
-    },
-  ];
-  
+  {
+    img: "/fruits.svg",
+    title: "Online food store",
+    description:
+      "This is an online store that sells food products. Customers can browse the selection of products online and place orders from the comfort of their homes.",
+    tech_stack: ["React", "Bootstrap"],
+    github_url: "",
+    demo_url: "https://exampleseven.eolsolutions.com.co/",
+  },
+  {
+    img: "/TwitchAPI.png",
+    title: "Twitch API Request",
+    description:
+      "Twitch API request to show the top 10 most viewed games on Twitch and the platform's real-time game search engine.",
+    tech_stack: ["ReactJs", "Tailwind", "Mobile First", "Axios", "API"],
+    github_url: "https://github.com/alexcarvajal/ApiTwitch",
+    demo_url: "https://twitchapi.netlify.app/",
+  },
+  {
+    img: "/batabit.svg",
+    title: "Batabit Responsive",
+    description:
+      "It is an application made in HTML and CSS with the mobile first concept that shows a trading landing page.",
+    tech_stack: ["HTML", "CSS", "Mobile First"],
+    github_url: "https://github.com/alexcarvajal/MobileFirst",
+    demo_url: "https://mobilityfirst.netlify.app/",
+  },
+  {
+    img: "/Ecommerce.png",
+    title: "Ejemplo Ecommerce",
+    description:
+      "Example of an Ecommerce with store functionalities, login, product selection and shopping cart.",
+    tech_stack: ["ReactJs", "Tailwind", "ReactNative"],
+    github_url: "https://github.com/alexcarvajal/Ecommerce",
+    demo_url: "https://commerce-tailw.netlify.app/",
+  },
+
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
